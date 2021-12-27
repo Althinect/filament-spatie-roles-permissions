bf9be901b27014f0346756f39304903c3637aed4
Tharinda Rodrigo <tharinda.rodrigo@pictureworks.com.au>
2021-12-28 03:15:43 +0530

    Added PermissionRelationManager for Roles

---
+++ src/Resources/RoleResource/RelationManager/PermissionRelationManager.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Althinect\FilamentSpatieRolesPermissions\Resources\RoleResource\RelationManager;
+
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\Form;
+use Filament\Resources\RelationManagers\BelongsToManyRelationManager;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+
+class PermissionRelationManager extends BelongsToManyRelationManager
+{
+    protected static string $relationship = 'permissions';
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                TextInput::make('guard_name')
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('guard_name'),
+
+            ])
+            ->filters([
+                //
+            ]);
+    }
+}
