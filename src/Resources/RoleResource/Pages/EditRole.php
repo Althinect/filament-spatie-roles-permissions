8cb16d1be4c0275d21eb6192eab26246b80a8cf1
tharindarodrigo <tharinda.rodrigo@gmail.com>
2023-12-03 10:51:20 -0700

    chore: added view Action

--- src/Resources/RoleResource/Pages/EditRole.php
+++ src/Resources/RoleResource/Pages/EditRole.php
@@ -4,6 +4,7 @@ namespace Althinect\FilamentSpatieRolesPermissions\Resources\RoleResource\Pages;
 
 use Althinect\FilamentSpatieRolesPermissions\Resources\RoleResource;
 use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditRole extends EditRecord
@@ -13,6 +14,7 @@ class EditRole extends EditRecord
     public function getHeaderActions(): array
     {
         return [
+            ViewAction::make(),
             DeleteAction::make(),
         ];
     }
