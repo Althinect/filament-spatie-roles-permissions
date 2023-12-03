d564efe19bb61c38f9e4f6111e325a7ec05b6471
tharindarodrigo <tharinda.rodrigo@gmail.com>
2023-12-03 11:05:17 -0700

    refactor: added return type

--- src/FilamentSpatieRolesPermissionsFacade.php
+++ src/FilamentSpatieRolesPermissionsFacade.php
@@ -4,9 +4,6 @@ namespace Althinect\FilamentSpatieRolesPermissions;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @see \Althinect\FilamentSpatieRolesPermissions\Skeleton\SkeletonClass
- */
 class FilamentSpatieRolesPermissionsFacade extends Facade
 {
     /**
@@ -14,7 +11,7 @@ class FilamentSpatieRolesPermissionsFacade extends Facade
      *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'filament-spatie-roles-permissions';
     }
