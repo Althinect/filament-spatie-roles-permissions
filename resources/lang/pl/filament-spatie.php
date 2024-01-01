94311a565e551724b20d1a2d15c623a08a03cb48
Bartosz Bujak <bartosz.bujak@me.com>
2024-01-01 16:43:12 +0100

    Polish translation added

---
+++ resources/lang/pl/filament-spatie.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Fields
+    |--------------------------------------------------------------------------
+    */
+
+    'field.guard_name' => 'Ochrona (guard)',
+    'field.permissions_count' => 'Ilość uprawnień',
+    'field.name' => 'Nazea',
+    'field.permissions' => 'Uprawnienia',
+    'field.roles' => 'Role',
+    'field.role' => 'Rola',
+    'field.team' => 'Zespół',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Labels
+    |--------------------------------------------------------------------------
+    */
+
+    'section.permission' => 'Uprawnienie',
+    'section.permissions' => 'Uprawnienia',
+    'section.role' => 'Rola',
+    'section.roles' => 'Role',
+    'section.roles_and_permissions' => 'ROle i Uprawnienia',
+    'select-team' => 'Wybierz Zespół',
+    'select-team-hint' => 'Pozostaw puste dla roli globalnej',
+    'section.users' => 'Użytkownicy',
+];
