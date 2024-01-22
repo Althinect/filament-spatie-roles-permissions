32b9440a4fd7c2686b7888447a35ef2d975debda
CodeTechNL <65519422+CodeTechNL@users.noreply.github.com>
2024-01-22 17:19:04 +0100

    Enhance namespace resolver (#140)
    * Fix namespace resolver
    
    * Remove PHP tags from filecontents

--- src/Commands/Permission.php
+++ src/Commands/Permission.php
@@ -239,6 +239,7 @@ class Permission extends Command
         $handle = fopen($file, 'r');
         if ($handle) {
             while (($line = fgets($handle)) !== false) {
+                $line = str($line)->remove('<?php')->remove('<?')->trim();
                 if (str_starts_with($line, 'namespace')) {
                     $parts = explode(' ', $line);
                     $ns = rtrim(trim($parts[1]), ';');
