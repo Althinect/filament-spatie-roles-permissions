f01d982e7faae289b95c3d1873f142bd36f358cd
Jamie Collins <jamie@wearearch.com>
2024-10-03 15:42:19 +0100

    Excluded models to be removed from the filters in resources

--- src/Commands/Permission.php
+++ src/Commands/Permission.php
@@ -31,7 +31,6 @@ class Permission extends Command
     {
         parent::__construct();
         $this->config = config('filament-spatie-roles-permissions.generator');
-
     }
 
     /**
@@ -53,7 +52,7 @@ class Permission extends Command
         $permissionModel = config('permission.models.permission');
 
         foreach ($this->permissions as $permission) {
-            $this->comment('Syncing Permission for: '.$permission['name']);
+            $this->comment('Syncing Permission for: ' . $permission['name']);
             $permissionModel::firstOrCreate($permission);
         }
     }
@@ -88,7 +87,7 @@ class Permission extends Command
             $modelName = $model->getShortName();
 
             $stub = '/stubs/genericPolicy.stub';
-            $contents = $filesystem->get(__DIR__.$stub);
+            $contents = $filesystem->get(__DIR__ . $stub);
 
             foreach ($this->permissionAffixes() as $key => $permissionAffix) {
                 foreach ($this->guardNames() as $guardName) {
@@ -100,7 +99,7 @@ class Permission extends Command
                     ];
 
                     if ($this->option('policies')) {
-                        $contents = Str::replace('{{ '.$key.' }}', $permission, $contents);
+                        $contents = Str::replace('{{ ' . $key . ' }}', $permission, $contents);
                     }
                 }
             }
@@ -108,7 +107,7 @@ class Permission extends Command
             if ($this->option('policies') || $this->option('yes-to-all')) {
 
                 $policyVariables = [
-                    'class' => $modelName.'Policy',
+                    'class' => $modelName . 'Policy',
                     'namespacedModel' => $model->getName(),
                     'namespacedUserModel' => (new ReflectionClass($this->config['user_model']))->getName(),
                     'namespace' => $this->config['policies_namespace'],
@@ -121,20 +120,20 @@ class Permission extends Command
                     if ($modelName == 'User' && $search == 'namespacedModel') {
                         $contents = Str::replace('use {{ namespacedModel }};', '', $contents);
                     } else {
-                        $contents = Str::replace('{{ '.$search.' }}', $replace, $contents);
+                        $contents = Str::replace('{{ ' . $search . ' }}', $replace, $contents);
                     }
                 }
 
-                if ($filesystem->exists(app_path('Policies/'.$modelName.'Policy.php'))) {
+                if ($filesystem->exists(app_path('Policies/' . $modelName . 'Policy.php'))) {
                     if ($this->option('oep')) {
-                        $filesystem->put(app_path('Policies/'.$modelName.'Policy.php'), $contents);
-                        $this->comment('Overriding Existing Policy: '.$modelName);
+                        $filesystem->put(app_path('Policies/' . $modelName . 'Policy.php'), $contents);
+                        $this->comment('Overriding Existing Policy: ' . $modelName);
                     } else {
-                        $this->warn('Policy already exists for: '.$modelName);
+                        $this->warn('Policy already exists for: ' . $modelName);
                     }
                 } else {
-                    $filesystem->put(app_path('Policies/'.$modelName.'Policy.php'), $contents);
-                    $this->comment('Creating Policy: '.$modelName);
+                    $filesystem->put(app_path('Policies/' . $modelName . 'Policy.php'), $contents);
+                    $this->comment('Creating Policy: ' . $modelName);
                 }
             }
         }
@@ -164,12 +163,12 @@ class Permission extends Command
 
             foreach ($resources as $resource) {
                 $resourceNameSpace = $this->extractNamespace($resource);
-                $reflection = new ReflectionClass($resourceNameSpace.'\\'.$resource->getFilenameWithoutExtension());
+                $reflection = new ReflectionClass($resourceNameSpace . '\\' . $resource->getFilenameWithoutExtension());
                 if (
                     ! $reflection->isAbstract() && $reflection->getParentClass() &&
                     $reflection->getParentClass()->getName() == 'Filament\Resources\Resource'
                 ) {
-                    $models[] = new ReflectionClass(app($resourceNameSpace.'\\'.$resource->getFilenameWithoutExtension())->getModel());
+                    $models[] = new ReflectionClass(app($resourceNameSpace . '\\' . $resource->getFilenameWithoutExtension())->getModel());
                 }
             }
         }
@@ -191,12 +190,11 @@ class Permission extends Command
 
         foreach ($files as $file) {
             $namespace = $this->extractNamespace($file);
-            $class = $namespace.'\\'.$file->getFilenameWithoutExtension();
+            $class = $namespace . '\\' . $file->getFilenameWithoutExtension();
             $model = new ReflectionClass($class);
             if (! $model->isAbstract()) {
                 $models[] = $model;
             }
-
         }
 
         return $models;
@@ -255,7 +253,8 @@ class Permission extends Command
     {
         $models = $this->getModels();
         $customModels = $this->getCustomModels();
+        $excludedModels = $this->getExcludedModels();
 
-        return array_merge($models, $customModels);
+        return array_diff(array_merge($models, $customModels), $excludedModels);
     }
 }
