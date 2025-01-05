123a51acc87ba24102b006560ca1a30c037383c8
Gabriel Patzleiner <gabriel.patzleiner@gmail.com>
2025-01-05 20:42:52 +0100

    Fixed exception when user_name_searchable_columns is null

--- src/Resources/RoleResource/RelationManager/UserRelationManager.php
+++ src/Resources/RoleResource/RelationManager/UserRelationManager.php
@@ -58,7 +58,7 @@ class UserRelationManager extends RelationManager
             ->columns([
                 TextColumn::make(config('filament-spatie-roles-permissions.user_name_column'))
                     ->label(__('filament-spatie-roles-permissions::filament-spatie.field.name'))
-                    ->searchable(config('filament-spatie-roles-permissions.user_name_searchable_columns', 'name')),
+                    ->searchable(config('filament-spatie-roles-permissions.user_name_searchable_columns', 'name') ?? true),
             ])
             ->filters([
 
