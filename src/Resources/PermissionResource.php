2c6adbd173857fbd3475c5fd04281f9e7dd68df1
Santhosh Sivan S <86248110+SanthoshSivan-Dev@users.noreply.github.com>
2025-03-27 15:17:58 +0530

    Update PermissionResource.php

--- src/Resources/PermissionResource.php
+++ src/Resources/PermissionResource.php
@@ -30,7 +30,7 @@ class PermissionResource extends Resource
 {
     public static function isScopedToTenant(): bool
     {
-        return config('filament-spatie-roles-permissions.scope_to_tenant', true);
+        return config('filament-spatie-roles-permissions.scope_to_tenant.permissions', true);
     }   
 
     public static function getNavigationIcon(): ?string
