f6b05e4f1bdbafe7d002671ec3f2ed8f908efa97
Roman Borkunov <27624002+broqit@users.noreply.github.com>
2025-06-16 22:08:49 +0300

    Update UserRelationManager.php

--- src/Resources/RoleResource/RelationManager/UserRelationManager.php
+++ src/Resources/RoleResource/RelationManager/UserRelationManager.php
@@ -5,8 +5,8 @@ namespace Althinect\FilamentSpatieRolesPermissions\Resources\RoleResource\Relati
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Schemas\Schema;
-use Filament\Tables\Actions\AttachAction;
-use Filament\Tables\Actions\DetachAction;
+use Filament\Actions\AttachAction;
+use Filament\Actions\DetachAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Model;
