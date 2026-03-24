65d6580d8bcba2b56335b70d0f01bffda0fa2ad7
Kris <kris@libereat.com>
2026-03-24 10:49:07 +0000

    bugfix/3.x-view-role-column-span
    * make the container for the Roles form use columnSpanFull to ensure it spans even when using a custom theme.

--- src/Resources/RoleResource.php
+++ src/Resources/RoleResource.php
@@ -120,7 +120,7 @@ class RoleResource extends Resource
                                     ->placeholder(__('filament-spatie-roles-permissions::filament-spatie.select-team'))
                                     ->hint(__('filament-spatie-roles-permissions::filament-spatie.select-team-hint')),
                             ]),
-                    ]),
+                    ])->columnSpanFull(),
             ]);
     }
 
